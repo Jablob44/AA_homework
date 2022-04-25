cf3d00693af42cec078e5a52a3722104e7985113
Jacob <jacob@squaw.org>
2022-04-25 15:23:47 -0700

    bootcampProj

---
+++ Jacob_Justice_bootcamp_project/lib/bootcamp.rb
@@ -0,0 +1,76 @@
+class Bootcamp
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new {|hash, k| hash[k] = []}
+    end
+    
+    def name()
+        @name
+    end
+
+    def slogan()
+        @slogan
+    end
+
+    def teachers()
+        @teachers
+    end
+
+    def students()
+        @students
+    end
+
+    def hire(teacher)
+        @teachers << teacher
+    end
+
+    def enroll(student) 
+        if students.length < @student_capacity
+            @students << student
+            return true
+        else
+            return false
+        end
+    end
+
+    def enrolled?(student)
+        @students.include?(student)
+    end
+
+    def student_to_teacher_ratio()
+        (@students.length/@teachers.length).floor
+    end
+
+    def add_grade(student, grade)
+        if @students.include?(student)
+            @grades[student] << grade
+            return true
+        else
+            return false
+        end
+    end
+
+    def num_grades(student)
+        @grades[student].length
+    end
+
+    def average_grade(student)
+        if @students.include?(student) && @grades[student].length != 0
+            return grade_total(student)/num_grades(student)
+        else
+            return nil
+        end
+    end
+
+    def grade_total(student)
+        total = 0
+        @grades[student].each do |grade|
+            total += grade
+        end
+        total
+    end
+end
