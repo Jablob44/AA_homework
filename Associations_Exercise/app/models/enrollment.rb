675d101ff6f04a478e1a3e1c30bade8e2375c199
Zhile Lin <zhlin04@gmail.com>
2022-05-19 14:39:26 -0700

    finished enrollment model association

--- Associations_Exercise/app/models/enrollment.rb
+++ Associations_Exercise/app/models/enrollment.rb
@@ -1,2 +1,13 @@
 class Enrollment < ApplicationRecord
+
+      has_many :user,
+         primary_key: :id,
+         foreign_key: :student_id,
+         class_name: :User
+
+      has_many :course,
+         primary_key: :id,
+         foreign_key: :course_id,
+         class_name: :Course
+      
 end
