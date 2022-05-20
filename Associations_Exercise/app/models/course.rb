60cd1da4ffe38857e97b10b9dee10070bfb966de
Jacob <jacob@squaw.org>
2022-05-19 17:03:58 -0700

    fixed version of Associations

--- Associations_Exercise/app/models/course.rb
+++ Associations_Exercise/app/models/course.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
     has_many :enrollments,
         primary_key: :id,
@@ -11,8 +22,8 @@ class Course < ApplicationRecord
         primary_key: :id,
         foreign_key: :prereq_id,
         class_name: :Course
-    has_many :instructor, 
-        through: :enrollments, 
+    belongs_to :instructor, 
+        primary_key: :id,
         foreign_key: :instructor_id,
-        source: :user
+        class_name: :User
 end
