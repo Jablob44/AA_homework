4fff146a864bd28b39448f823024b5675cfb8f19
alan <alan001tk@gmail.com>
2022-06-09 14:29:49 -0700

    Finished addNumbers

--- W8D4/introToCallbacks.js
+++ W8D4/introToCallbacks.js
@@ -35,4 +35,27 @@ class Clock {
     }
   }
   
-  const clock = new Clock();
+//  const clock = new Clock();
+
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function addNumbers(sum = 0, numsLeft, completionCallback) {
+  if (numsLeft > 0) {
+    reader.question("Enter a number ", function (num) {
+      sum += parseInt(num);
+      addNumbers(sum, --numsLeft, completionCallback);
+      if (numsLeft === 0) {
+        reader.close();
+      };
+    });
+  } else {
+    completionCallback(sum);
+  };
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
